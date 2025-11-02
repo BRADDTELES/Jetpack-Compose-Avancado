884caab2be08db4e4e7014a8f6d84ae906eabbac
BRADDTELES <braddteles@gmail.com>
2025-11-02 11:26:10 -0300

    Praticando o uso de Box
    - [x] compreendendo o uso de caixas ou container, que é o Box

--- StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
+++ StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
@@ -5,37 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,48 +31,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Home() {
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text( text = "Stack Mobile" )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Green
-                ),
-                actions = {
-                    Icon(
-                        Icons.Rounded.Search,
-                        null,
-                        Modifier.padding(2.dp)
-                    )
-                    Icon(
-                        Icons.Rounded.Home,
-                        null,
-                        Modifier.padding(16.dp)
-                    )
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.Green
-            ) { }
-        }
-    ) { paddingValues ->
-
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.White)
+    ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding( paddingValues ),
-            Arrangement.Center,
-            Alignment.CenterHorizontally
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text("Danillo", fontSize = 20.sp)
-            Text("Danillo", fontSize = 20.sp)
-            Text("Danillo", fontSize = 20.sp)
+            Text("Danillo")
+            Text("Danillo")
+            Text("Danillo")
         }
-
     }
 
 }
