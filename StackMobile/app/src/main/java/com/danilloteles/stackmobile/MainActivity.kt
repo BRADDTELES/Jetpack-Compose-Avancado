45892c2fcd17e7db7758f40d6dadc346fb068d91
BRADDTELES <braddteles@gmail.com>
2025-11-02 11:08:36 -0300

    Praticando o uso de Scaffold
    - [x] adicionando a dependencia de Icons: `implementation("androidx.compose.material:material-icons-extended:1.7.8")`
    - [x] implementando os componentes dentro de Scaffold: TopAppBar, BottomAppBar
    - [x] Ícones de Home e Pesquisa, título de cabeçalho, cor de fundo na topBar.

--- StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
+++ StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
@@ -12,14 +12,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,37 +48,53 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun Home() {
 
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Surface(
-            modifier = Modifier.size(100.dp).weight(3f),
-            color = Color.Green
-        ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text( text = "Stack Mobile" )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Green
+                ),
+                actions = {
+                    Icon(
+                        Icons.Rounded.Search,
+                        null,
+                        Modifier.padding(2.dp)
+                    )
+                    Icon(
+                        Icons.Rounded.Home,
+                        null,
+                        Modifier.padding(16.dp)
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color.Green
+            ) { }
+        }
+    ) { paddingValues ->
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding( paddingValues ),
+            Arrangement.Center,
+            Alignment.CenterHorizontally
+        ) {
+            Text("Danillo", fontSize = 20.sp)
+            Text("Danillo", fontSize = 20.sp)
+            Text("Danillo", fontSize = 20.sp)
         }
 
-        Surface(
-            modifier = Modifier.size(100.dp).weight(1f),
-            color = Color.Blue
-        ) {
-
-        }
-/*
-        Surface(
-            modifier = Modifier.size(100.dp),
-            color = Color.Red
-        ) {
-
-        }*/
     }
 
 }
