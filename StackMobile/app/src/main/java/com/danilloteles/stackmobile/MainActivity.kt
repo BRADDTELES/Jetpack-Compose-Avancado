22c4e9fb2ace17c75c50153b94cc0de358b27876
BRADDTELES <braddteles@gmail.com>
2025-11-02 11:37:57 -0300

    Usando o Preview
    - [x] descobrindo o uso do Preview

--- StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
+++ StackMobile/app/src/main/java/com/danilloteles/stackmobile/MainActivity.kt
@@ -5,15 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,21 +35,39 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
 @Composable
 fun Home() {
 
-    Box(
-        modifier = Modifier.fillMaxSize().background(Color.White)
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Green),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text("Danillo")
-            Text("Danillo")
-            Text("Danillo")
-        }
+        Icon(
+            Icons.Rounded.Home,
+            contentDescription = null,
+            tint = Color.Blue,
+            modifier = Modifier.size(200.dp).padding(10.dp)
+        )
+        Text("Stack Mobile", fontSize = 30.sp)
     }
 
+}
+
+@Composable
+fun Cadastro() {
+
+}
+
+@Preview
+@Composable
+fun CadastroPreview() {
+    Cadastro()
+}
+
+
+@Preview(name = "HomePreview")
+@Composable
+fun HomePreview() {
+    Home()
 }
