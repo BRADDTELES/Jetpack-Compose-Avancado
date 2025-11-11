008aeaec5c0fa21bb8b10678831610cfabd15056
BRADDTELES <braddteles@gmail.com>
2025-11-10 23:21:37 -0300

    Listando todos os contatos, usando o LaunchedEffect em vez do jeito que o professor colocou.

--- AgendadeContatos/app/src/main/java/com/danillotelescarneiro/agendadecontatos/dao/ContatoDao.kt
+++ AgendadeContatos/app/src/main/java/com/danillotelescarneiro/agendadecontatos/dao/ContatoDao.kt
@@ -2,6 +2,7 @@ package com.danillotelescarneiro.agendadecontatos.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.danillotelescarneiro.agendadecontatos.model.Contato
 
 @Dao
@@ -9,5 +10,8 @@ interface ContatoDao {
 
     @Insert
     fun gravar(contato: Contato)
+
+    @Query("SELECT * FROM tabela_contatos ORDER BY nome ASC")
+    suspend fun getContatos(): MutableList<Contato>
 
 }
--- AgendadeContatos/app/src/main/java/com/danillotelescarneiro/agendadecontatos/views/ListaContatos.kt
+++ AgendadeContatos/app/src/main/java/com/danillotelescarneiro/agendadecontatos/views/ListaContatos.kt
@@ -1,12 +1,11 @@
 package com.danillotelescarneiro.agendadecontatos.views
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,11 +16,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.danillotelescarneiro.agendadecontatos.AppDatabase
+import com.danillotelescarneiro.agendadecontatos.itemlista.ContatoItem
+import com.danillotelescarneiro.agendadecontatos.model.Contato
 import com.danillotelescarneiro.agendadecontatos.ui.theme.PURPLE_500
 import com.danillotelescarneiro.agendadecontatos.ui.theme.WHITE
 
@@ -30,6 +37,18 @@ import com.danillotelescarneiro.agendadecontatos.ui.theme.WHITE
 fun ListaContatos(
     navController: NavController
 ) {
+
+    val listaContatos = remember { mutableStateListOf<Contato>() }
+
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        val contatoDao = AppDatabase.getInstance(context).contatoDao()
+        val contatosDoBanco = contatoDao.getContatos()
+
+        listaContatos.clear()
+        listaContatos.addAll(contatosDoBanco)
+    }
 
     Scaffold(
         topBar = {
@@ -60,11 +79,13 @@ fun ListaContatos(
         }
     ) { paddingValues ->
 
-        Column(
-            modifier = Modifier.fillMaxSize()
-                .padding(paddingValues).background(WHITE).verticalScroll(rememberScrollState())
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().background(Color.LightGray)
+                .padding(paddingValues)
         ) {
-
+            itemsIndexed(listaContatos){ _, contato ->
+                ContatoItem(contato = contato)
+            }
         }
 
     }
